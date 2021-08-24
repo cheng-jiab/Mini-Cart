299e8ecc490fd9e3a4876a184ce0d9f1879e9bae
cheng-jiab <cheng.jiab@husky.neu.edu>
2021-08-23 23:18:23 -0400

    Created products page, shown products only related with the specifit category.

--- miniCart/urls.py
+++ miniCart/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 #from miniCart.settings import MEDIA_ROOT
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.urls.conf import include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from category.models import Category
+from django.shortcuts import get_object_or_404, render
+from .models import Product
+from category.models import Category
 
 # Create your views here.
+
+def store(request, categorySlug=None):
+    categories = None
+    products = None
+
+    if categorySlug != None:
+        categories = get_object_or_404(Category, slug=categorySlug)
+        products = Product.objects.filter(category=categories, isAvailable=True)
+        productCount = products.count()
+    else:
+        products = Product.objects.all().filter(isAvailable=True)
+        productCount = products.count()
+    
+    context = {
+        'products' : products,
+        'productCount' : productCount,
+
+    }
+    
+    return render(request,'store/store.html', context)
